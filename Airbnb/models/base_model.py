5de49ff8246cfa208c4bb1e1b1a8f0f4fafb504b
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-04-10 02:16:47 +0000

    task 3

--- Airbnb/models/base_model.py
+++ Airbnb/models/base_model.py
@@ -13,5 +13,16 @@ class BaseModel():
     def __str__(self):
 #prints attribute; classname, id, dict
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        self.updated_at = datetime.now()
 
+#makes a copy of a dictionary
+
+    def to_dict(self):
+        copy_dict = self.__dict__.copy()
+        copy_dict["__class__"] = self.__class__.__name__
+        copy_dict["created_at"] = self.created_at.isoformat()
+        copy_dict["updated_at"] = self.updated_at.isoformat()
+        return copy_dict
 
