1dde8ccec28c9ece5e7e358e607530c831da6c04
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-04-16 17:17:58 +0000

    airbnb

--- Airbnb/engine/file_storage.py
+++ Airbnb/engine/file_storage.py
@@ -8,6 +8,22 @@ class FileStorage:
 		return ("self.__objects")
 	
 	def new(self, obj):
-		self.__objects = obj.__class__name
-		
-	
+		key = f"{obj.__class__.__ name__}.{obj.id}"
+		self.__objects[key] = obj
+
+	def save(self):
+		with open('self.__file__path', 'w', encoding='utf-8') as f
+		my_d = {k: v.to_dict for k, v in self.__object.item():}
+		json.dump(my_d, f)
+
+	def reload(self):
+		try:
+			with open(self.__file__path, 'r', encoding='utf-8') as f
+			mamuro = json.load(f)
+			for v in mamuro.value():
+			cls_name = v["__class__"]
+			del v["__class_"_]
+			self.new(eval(f"{cls_name}") (**v))
+		except FileNotFoundError:
+			return
+
